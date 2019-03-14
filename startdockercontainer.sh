5a250645a69d070ab71fa9a785820051583113d1
ben <2088471+f-ben@users.noreply.github.com>
2019-03-14 22:39:37 +0100

    Update startdockercontainer.sh

--- startdockercontainer.sh
+++ startdockercontainer.sh
@@ -4,4 +4,4 @@ echo "Starting docker container..."
 echo "Using ports 14265/tcp 14600/udp 15600/tcp"
 echo "Using volume /mnt/usb/rock64iri/data"
 echo "-"
-docker run --name iri -d -p 14265:14265 -p 14600:14600/udp -p 15600:15600 -v /mnt/usb/rock64iri/data:/data fben/rock64iri:1.6.1-RELEASE
+docker run --restart always --name iri -d -p 14265:14265 -p 14600:14600/udp -p 15600:15600 -v /mnt/usb/rock64iri/data:/data fben/rock64iri:1.6.1-RELEASE
