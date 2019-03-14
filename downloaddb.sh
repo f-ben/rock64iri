6db4e8b22f8c5ba31cc3bf962fe5fba0ba1047ac
ben <2088471+f-ben@users.noreply.github.com>
2019-03-14 22:25:59 +0100

    Update downloaddb.sh

--- downloaddb.sh
+++ downloaddb.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 echo "Deleting old database..."
-rm -rf ./data/iri/mainnet*
-rm -rf ./data/iri/spent-addresses-db
+rm -rf ./data/mainnet*
+rm -rf ./data/spent-addresses-db
 echo "Downloading new database..."
 curl https://x-vps.com/iota.db.tgz -o iota.db.tgz
 echo "Extracting file..."
-tar -xf iota.db.tgz -C ./data/iri
+tar -xf iota.db.tgz -C ./data
 echo "Removing downloaded file..."
 rm iota.db.tgz
 echo "...finished!"
