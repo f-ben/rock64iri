1fde3cc316c024e1f5318d82aab653e62f2a7361
ben <2088471+f-ben@users.noreply.github.com>
2019-03-15 01:41:57 +0100

    Create getnodeinfo.sh

---
+++ getnodeinfo.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+curl -s http://127.0.0.1:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '{"command": "getNeighbors"}' | jq
+curl -s http://127.0.0.1:14265 -X POST -H 'Content-Type: application/json' -H 'X-IOTA-API-Version: 1' -d '{"command": "getNodeInfo"}' | jq
