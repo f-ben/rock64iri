606ca47863a2e072327598394c78afadc5ea374f
ben <2088471+f-ben@users.noreply.github.com>
2019-03-17 18:41:54 +0100

    Update and rename start.sh to run.sh

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ echo "Using volume /mnt/usb/rock64iri/data"
 echo "-"
 echo "This file should only be used for the initial setup."
 echo "If you already created your IRI container, please use"
-echo "docker start iri"
+echo "./start.sh"
 echo "instead."
 echo "-"
 docker run --restart always --name iri -d -p 14265:14265 -p 14600:14600/udp -p 15600:15600 -v /mnt/usb/rock64iri/data:/data fben/rock64iri:1.6.1-RELEASE
