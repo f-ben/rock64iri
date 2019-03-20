cc8df66d630f69c9685d5877e3a824423533e403
ben <2088471+f-ben@users.noreply.github.com>
2019-03-20 01:11:46 +0100

    Update downloaddb.sh

--- downloaddb.sh
+++ downloaddb.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-
+DATA=./data
+SNAP=./snapshots
 echo "Stopping IRI container..."
 ./stop.sh
 echo "Deleting old database..."
-rm -rf ./data/mainnet*
-rm -rf ./data/spent-addresses-db
+sudo rm -rf $DATA/mainnet*
+sudo rm -rf $SNAP/*
 echo "Downloading new database..."
 curl https://x-vps.com/iota.db.tgz -o iota.db.tgz
-echo "Extracting file..."
-tar -xf iota.db.tgz -C ./data
+echo "Extracting file and setting permissions..."
+sudo tar -xf iota.db.tgz -C $SNAP
+sudo chown -R root:root $SNAP
+sudo chmod -R 755 $SNAP
 echo "Removing downloaded file..."
 rm iota.db.tgz
 echo "Starting IRI container..."
