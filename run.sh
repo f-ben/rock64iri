5b4dc5cfc5161c629d9acc403f8cfa4d0d74c6a2
ben <2088471+f-ben@users.noreply.github.com>
2019-03-20 23:32:57 +0100

    Update run.sh
    fix local snapshots, volumes no longer needed since snapshots are stored in data/snapshots

--- run.sh
+++ run.sh
@@ -2,13 +2,14 @@
 
 ROCK64IRI=/mnt/usb/rock64iri
 ROCK64IRIDATA=$ROCK64IRI/data
-ROCK64IRISNAP=$ROCK64IRI/snapshots
+APIPORT=14265
+UDPPORT=14600
+TCPPORT=15600
 
 echo "Starting docker container..."
 echo "-"
-echo "Using ports 14265/tcp 14600/udp 15600/tcp"
+echo "Using ports $APIPORT $UDPPORT $TCPPORT"
 echo "Using volume $ROCK64IRIDATA"
-echo "Using volume $ROCK64IRISNAP"
 echo "-"
 echo "This file should only be used for the initial setup."
 echo "If you already created your IRI container, please use"
@@ -16,11 +17,8 @@ echo "./start.sh"
 echo "instead. Starting container, please wait..."
 echo "-"
 docker run -d --restart always --name iri \
-           -p 14265:14265 \
-           -p 14600:14600/udp \
-           -p 15600:15600 \
+           -p $APIPORT:$APIPORT \
+           -p $UDPPORT:$UDPPORT \
+           -p $TCPPORT:$TCPPORT \
            -v $ROCK64IRIDATA:/data \
-           -v $ROCK64IRISNAP/mainnet.snapshot.meta:/snapshots/mainnet.snapshot.meta \
-           -v $ROCK64IRISNAP/mainnet.snapshot.state:/snapshots/mainnet.snapshot.state \
-           -v $ROCK64IRISNAP/spent-addresses-db:/snapshots/spent-addresses-db \
            fben/rock64iri:1.6.1-RELEASE
